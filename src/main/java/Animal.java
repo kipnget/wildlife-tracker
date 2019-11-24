0b49c7edf792ca706e2a731f432de27c4830d321
MUTAI <yegoallen@gmail.com>
2019-11-24 18:25:47 +0300

    added abstract class which implements database management

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -1,2 +1,26 @@
-public class Animal {
-}
+
+
+
+public abstract class Animal implements DatabaseManagement {
+    protected int id;
+    protected String name;
+    protected String type;
+    private static final int MIN_NAME_LENGTH = 1;
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        if(DatabaseManagement.nameValidation(name)) {
+            this.name = name;
+        }
+    }
+
+    public String getType() {
+        return this.type;
+    }
