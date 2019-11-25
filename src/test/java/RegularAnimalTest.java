ef893c8e1a89e8408a8b4b3b34b851944e612193
MUTAI <yegoallen@gmail.com>
2019-11-25 20:07:33 +0300

    added test for regular animal name

--- src/test/java/RegularAnimalTest.java
+++ src/test/java/RegularAnimalTest.java
@@ -1,6 +1,7 @@
 import org.junit.Rule;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class RegularAnimalTest {
@@ -14,4 +15,63 @@ public class RegularAnimalTest {
         RegularAnimal testAnimal = new RegularAnimal("Rabbit");
         assertTrue(testAnimal instanceof RegularAnimal);
     }
+
+    @Test
+    public void animal_instantiatesWithoutId_0() {
+        RegularAnimal testAnimal = new RegularAnimal("Rabbit");
+        assertEquals( 0, testAnimal.getId());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void animal_cannotInstantiateEmptyName_IllegalArgumentException() {
+        RegularAnimal testAnimal = new RegularAnimal("");
+    }
+
+    // Name
+    @Test
+    public void animal_instantiatesWithName_Rabbit() {
+        RegularAnimal testAnimal = new RegularAnimal("Rabbit");
+        assertEquals("Rabbit", testAnimal.getName());
+    }
+
+    @Test
+    public void setName_setsANewName_Goat() {
+        RegularAnimal testAnimal = new RegularAnimal("Rabbit");
+        testAnimal.setName("Goat");
+        assertEquals("Goat", testAnimal.getName());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setName_cannotSetEmptyName_IllegalArgumentException() {
+        RegularAnimal testAnimal = new RegularAnimal("Rabbit");
+        testAnimal.setName("");
+    }
+
+    @Test
+    public void save_savesNameToDB_Rabbit() {
+        RegularAnimal testAnimal = new RegularAnimal("Rabbit");
+        testAnimal.save();
+        RegularAnimal savedRegularAnimal = RegularAnimal.find(testAnimal.getId());
+        assertEquals("Rabbit", savedRegularAnimal.getName());
+    }
+
+    @Test
+    public void update_preservesOriginalName_Rabbit() {
+        RegularAnimal testAnimal = new RegularAnimal("Rabbit");
+        testAnimal.save();
+        testAnimal.update();
+        RegularAnimal savedRegularAnimal = RegularAnimal.find(testAnimal.getId());
+        assertEquals("Rabbit", savedRegularAnimal.getName());
+    }
+
+    @Test
+    public void update_savesNewNameTODB_Goat() {
+        RegularAnimal testAnimal = new RegularAnimal("Rabbit");
+        testAnimal.save();
+        testAnimal.setName("Goat");
+        testAnimal.update();
+        RegularAnimal savedRegularAnimal = RegularAnimal.find(testAnimal.getId());
+        assertEquals("Goat", savedRegularAnimal.getName());
+    }
+
 }
