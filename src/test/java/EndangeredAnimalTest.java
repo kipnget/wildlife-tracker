0d2ab1fac629970835b22609b34380bb0ad3631a
MUTAI <yegoallen@gmail.com>
2019-11-25 19:52:29 +0300

    test for endangered animal to instantiate correctlty and expect true

--- src/test/java/EndangeredAnimalTest.java
+++ src/test/java/EndangeredAnimalTest.java
@@ -1,2 +1,17 @@
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
 public class EndangeredAnimalTest {
+
+    @Rule
+    public DatabaseRule database = new DatabaseRule();
+
+    //Instantiation
+    @Test
+    public void endangeredAnimal_instantiatesCorrectly_true() {
+        EndangeredAnimal testAnimal = new EndangeredAnimal("Rhino", 1.5, "Good");
+        assertTrue(testAnimal instanceof EndangeredAnimal);
+    }
 }
