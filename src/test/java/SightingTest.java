9489261e5b2889266b3f01b017bcadb43bf33dd3
MUTAI <yegoallen@gmail.com>
2019-11-25 19:58:34 +0300

    test for sighting to instantiate correctlty and expect true

--- src/test/java/SightingTest.java
+++ src/test/java/SightingTest.java
@@ -1,2 +1,33 @@
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.sql.Timestamp;
+
+import static org.junit.Assert.assertTrue;
+
 public class SightingTest {
+    private RegularAnimal testAnimal;
+    private Location testLocation;
+    private Ranger testRanger;
+
+    @Rule
+    public DatabaseRule database = new DatabaseRule();
+
+    @Before
+    public void setUp() {
+        testAnimal = new RegularAnimal("Rabbit");
+        testAnimal.save();
+        testLocation = new Location("Near bridge", 1.525, -2.311);
+        testLocation.save();
+        testRanger = new Ranger("User", "Mjanja", "Mwenyewe", 1, 5035550000l);
+        testRanger.save();
+    }
+
+    //Instantiation
+    @Test
+    public void sighting_instantiatesCorrectly_true() {
+        Sighting testSighting = new Sighting(testAnimal.getId(), testLocation.getId(), testRanger.getId(), new Timestamp(1L));
+        assertTrue(testSighting instanceof Sighting);
+    }
 }
