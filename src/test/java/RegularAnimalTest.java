9975cc4aa36e7a13ea68edd927a9b609a68e73b3
MUTAI <yegoallen@gmail.com>
2019-11-25 19:56:55 +0300

    test for regularanimal to instantiate correctlty and expect true

--- src/test/java/RegularAnimalTest.java
+++ src/test/java/RegularAnimalTest.java
@@ -1,2 +1,17 @@
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
 public class RegularAnimalTest {
+
+    @Rule
+    public DatabaseRule database = new DatabaseRule();
+
+    //Instantiation
+    @Test
+    public void animal_instantiatesCorrectly_true() {
+        RegularAnimal testAnimal = new RegularAnimal("Rabbit");
+        assertTrue(testAnimal instanceof RegularAnimal);
+    }
 }
