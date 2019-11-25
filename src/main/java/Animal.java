003b7dec197547993aabc7edbb50af3989b1c118
MUTAI <yegoallen@gmail.com>
2019-11-25 20:22:19 +0300

    added nameExists to animal.java to fix endangered animal names if exist in db

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -1,5 +1,5 @@
-
-
+import org.sql2o.Connection;
+import java.util.List;
 
 public abstract class Animal implements DatabaseManagement {
     protected int id;
@@ -16,7 +16,7 @@ public abstract class Animal implements DatabaseManagement {
     }
 
     public void setName(String name) {
-        if(DatabaseManagement.nameValidation(name)) {
+        if (DatabaseManagement.nameValidation(name)) {
             this.name = name;
         }
     }
@@ -24,3 +24,31 @@ public abstract class Animal implements DatabaseManagement {
     public String getType() {
         return this.type;
     }
+
+    public List<Sighting> getSightings() {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "SELECT * FROM sightings WHERE animal_id = :animal_id;";
+            return con.createQuery(sql)
+                    .addParameter("animal_id", this.id)
+                    .addColumnMapping("time_of_sighting", "timeOfSighting")
+                    .addColumnMapping("animal_id", "animalId")
+                    .addColumnMapping("location_id", "locationId")
+                    .addColumnMapping("ranger_id", "rangerId")
+                    .executeAndFetch(Sighting.class);
+        }
+    }
+
+    public static boolean nameExists(String name, int id) {
+        Integer count = 0;
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "SELECT count(name) FROM animals WHERE name = :name AND id != :id;";
+            count = con.createQuery(sql)
+                    .throwOnMappingFailure(false)
+                    .addParameter("name", name)
+                    .addParameter("id", id)
+                    .executeScalar(Integer.class);
+        }
+        return count != 0;
+    }
+
+}
