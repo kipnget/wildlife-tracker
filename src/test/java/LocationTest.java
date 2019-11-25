dbc1b889022c1e6c3b705f68b3a08ec8ee58036c
MUTAI <yegoallen@gmail.com>
2019-11-25 19:54:20 +0300

    test for location to instantiate correctlty and expect true

--- src/test/java/LocationTest.java
+++ src/test/java/LocationTest.java
@@ -1,2 +1,17 @@
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
 public class LocationTest {
+
+    @Rule
+    public DatabaseRule database = new DatabaseRule();
+
+    // Instantiation
+    @Test
+    public void ranger_instantiatesCorrectly_true() {
+        Location testLocation = new Location("Near bridge", 1.525, -2.311);
+        assertTrue(testLocation instanceof Location);
+    }
 }
