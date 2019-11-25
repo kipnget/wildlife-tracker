7e22ca46357ffa94c8c7856097ebc352783acea0
MUTAI <yegoallen@gmail.com>
2019-11-25 19:55:54 +0300

    test for ranger to instantiate correctlty and expect true

--- src/test/java/RangerTest.java
+++ src/test/java/RangerTest.java
@@ -1,2 +1,17 @@
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
 public class RangerTest {
+
+    @Rule
+    public DatabaseRule database = new DatabaseRule();
+
+    // Instantiation
+    @Test
+    public void ranger_instantiatesCorrectly_true() {
+        Ranger testRanger = new Ranger("User", "Mjanja", "Mwenyewe", 1, 5035550000L);
+        assertTrue(testRanger instanceof Ranger);
+    }
 }
